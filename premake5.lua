0ba0ca1221a29db23bc567e63db3dfa121280c0a
Ferhat TEPE <ferhattepee@gmail.com>
2024-05-19 11:21:19 +0300

    fix: premake issues

--- premake5.lua
+++ premake5.lua
@@ -1,62 +1,44 @@
 project "GLFW"
-    kind "StaticLib"
-    language "C"
+kind "StaticLib"
+language "C"
+staticruntime "off"
+warnings "off"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+targetdir("bin/" .. outputdir .. "/%{prj.name}")
+objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h"
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-        "src/input.c",
-        "src/monitor.c",
-        "src/vulkan.c",
-        "src/window.c"
-    }
+files {
+    "include/GLFW/glfw3.h", "include/GLFW/glfw3native.h", "src/glfw_config.h",
+    "src/context.c", "src/init.c", "src/input.c", "src/monitor.c",
+    "src/vulkan.c", "src/window.c"
+}
 
-    filter "system:windows"
-        buildoptions {"-std=c11","-lgdi32"}
-        systemversion "lastest"
-        staticruntine "on"
+filter "system:windows"
+systemversion "latest"
 
-        files
-        {
-            "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wg1_context.c",
-            "src/eg1_context.c",
-            "src/osmesa_context.c"
-        }
+files {
+    "src/win32_init.c", "src/win32_joystick.c", "src/win32_monitor.c",
+    "src/win32_time.c", "src/win32_thread.c", "src/win32_window.c",
+    "src/wgl_context.c", "src/egl_context.c", "src/osmesa_context.c"
+}
 
-        defines
-        {
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
-        }
+defines {"_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS"}
 
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
+filter "configurations:Debug"
+runtime "Debug"
+symbols "on"
 
-    filter { "system:windows", "configurations:Debug-AS" }
-        runtine "Debug"
-        symbols "on"
-        sanitize { "Address" }
-        flags {"NoRuntimeChecks", "NoIncrementalLink"}
+filter {"system:windows", "configurations:Debug-AS"}
+runtime "Debug"
+symbols "on"
+sanitize {"Address"}
+flags {"NoRuntimeChecks", "NoIncrementalLink"}
 
-    filter "configurations:Release"
-        runtine "Release"
-        optimize "speed"
+filter "configurations:Release"
+runtime "Release"
+optimize "speed"
 
 filter "configurations:Dist"
-        runtime "Release"
-        optimize "speed"
-        symbols "off"
+runtime "Release"
+optimize "speed"
+symbols "off"
